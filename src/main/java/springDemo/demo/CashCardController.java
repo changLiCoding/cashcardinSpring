daeffb3dc36f08e76759f617c9d7bb6646f20abf
javascriptsucks <evix1988@live.cn>
2023-07-22 02:12:00 -0600

    injection CashCardRepository

--- src/main/java/springDemo/demo/CashCardController.java
+++ src/main/java/springDemo/demo/CashCardController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    private CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
         if (requestedId.equals(99L)) {
